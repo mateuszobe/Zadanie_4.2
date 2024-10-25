cb13446746ed23d5628fa1b23aea42e9fe5476a7
Mateusz Oberski <mateuszoberski2@gmail.com>
2024-10-25 14:17:44 +0200

    Add examples

--- zadanie_4.2.py
+++ zadanie_4.2.py
@@ -5,3 +5,8 @@ def czy_slowo_jest_palindromem(word):
     Wynikiem jest bool - 'True' jeśli słowo jest palindromem i 'False' jeżeli nie jest.
     """
     return word == word[::-1]
+
+print(czy_slowo_jest_palindromem("kajak"))
+print(czy_slowo_jest_palindromem("potop"))
+print(czy_slowo_jest_palindromem("kot"))
+print(czy_slowo_jest_palindromem("pies"))
